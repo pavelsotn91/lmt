d27a2337bb1d01e5d225486fac4a309541926787
pavelsotn91 <pavelsotn91@gmail.com>
2019-01-16 21:37:35 +0300

    one more commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	//$.getJSON("https://spreadsheets.google.com/feeds/list/1J-soackv5COr5wQWB6UchwTwvPoWUUa4QgKxtCQSBh8/od6/public/values?alt=json", function(data) {
+		//data = data['feed']['entry'];
+		//console.log(data);
+		//showTours(data);
+	//});
+	
+	//function showTours(data) {
+		//var out = '';
+		//for(var i=0; i<data.length; i++) {
+			//out += '<div class="row tour">' + data[i]['gsx$города']['$t'] 
+			//+ '</div>';
+			//$('#tours').html(out);
+		//}
+	//}
+	console.log(1);
+});
+
+
+
+
